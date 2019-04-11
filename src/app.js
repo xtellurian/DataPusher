43d0acc4ea060ded431dfebdb97e187051be2674
Rian Finnegan <rian.finnegan@microsoft.com>
2019-04-11 14:28:13 +1000

    stringify the data

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ function sendData(data) {
     request.post({
         headers: { 'content-type': 'application/json', "Authorization": AUTH_HEADER },
         url: ENDPOINT,
-        body: data
+        body: JSON.stringify(data)
     }, function (error, response, body) {
         console.log("response body:")
         console.log(body)
