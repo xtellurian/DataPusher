2cbbd0021fa07605cadb12c124744ffd849f168c
Rian Finnegan <rian.finnegan@microsoft.com>
2019-04-11 13:10:03 +1000

    initial working commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+'use strict';
+require('dotenv').config()
+
+const fs = require('fs');
+const request = require('request');
+
+let ENDPOINT = process.env.ENDPOINT
+let AUTH_HEADER = process.env.AUTH_HEADER
+
+let rawdata = fs.readFileSync('data/data.json');
+let dataArray = JSON.parse(rawdata);
+
+function sendData(data) {
+    console.log("sending...")
+    console.log(data);
+
+    request.post({
+        headers: { 'content-type': 'application/json', "Authorization": AUTH_HEADER },
+        url: ENDPOINT,
+        form: data
+    }, function (error, response, body) {
+        console.log("response body:")
+        console.log(body)
+    });
+}
+
+function sendNext() {
+    if (index >= dataArray.length) {
+        index = 0;
+    }
+    let data = dataArray[index];
+    sendData(data);
+}
+
+var index = 0
+setInterval(function(){ sendNext() },1000)
